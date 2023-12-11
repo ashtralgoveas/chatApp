323a82e6f165e74cba02879952a13b6df5cb0e85
Ashtral Lorien Goveas <ashtralgoveas@gmail.com>
2023-12-11 10:10:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: "https://chat-app-ten-blond.vercel.app/",
+    origin: "https://chat-app-ten-blond.vercel.app",
     methods: ["GET", "POST"],
   },
 });
