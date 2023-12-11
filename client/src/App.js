bcdd6e6ebb3672b448ced6502ba6ace05dca35a6
Ashtral Lorien Goveas <ashtralgoveas@gmail.com>
2023-12-11 10:09:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import Chat from "./components/Chat";
 
 // const socket = io.connect("http://localhost:3001");
-const socket = io.connect("https://chat-app-iota-eight-77.vercel.app/");
+const socket = io.connect("https://chat-app-iota-eight-77.vercel.app");
 
 function App() {
   const [username, setUsername] = useState("");
